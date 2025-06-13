2f08dbd7cd5298e52714cb79fa715f6db155bfe5
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-06-13 19:08:32 +0530

    Add billing logic

---
+++ src/brewery.ts
@@ -0,0 +1,14 @@
+import { displayProducts } from "./utils/displayProducts";
+import { orderProduct, selectedItems } from "./utils/orderProduct";
+import { billing } from "./utils/billing";
+import { SelectedItem } from "./interfaces/orderItem";
+export async function startBilling():Promise<SelectedItem[]> {
+    console.log("Welcome to Brewery!\n");
+    console.log('Available Products in our Restarant :\n\n');
+
+    displayProducts();
+    orderProduct();
+    billing()
+return selectedItems;
+}
+startBilling();
